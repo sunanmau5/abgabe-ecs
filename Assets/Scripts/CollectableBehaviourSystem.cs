10dc019e79d5d4d73ef33ab745aca4b6a6b15fce
sunanmau5 <s0566144@htw-berlin.de>
2020-12-01 23:53:12 +0100

    Added SpawnCollectableSystem, CollectedComponent, CollectableBehaviourSystem and also rotation for the prefab

---
+++ Assets/Scripts/CollectableBehaviourSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Burst;
+using Unity.Mathematics;
+
+public class CollectableBehaviourSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        Entities.ForEach((ref Rotation rotate, in CollectedComponent collected) =>
+        {
+            if (collected.isCollected)
+            {
+                quaternion rot = quaternion.RotateX(math.radians(collected.radiansPerSecond * deltaTime));
+                rotate.Value = math.mul(rotate.Value, rot);
+            }
+        }).ScheduleParallel();
+    }
+}
---
+++ Assets/Scripts/SpawnCollectableSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class SpawnCollectableSystem : SystemBase
+{
+    BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        // Cache the BeginInitializationEntityCommandBufferSystem in a field, so we don't have to create it every frame
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+        Entities
+            .WithBurst(FloatMode.Default, FloatPrecision.Standard, true)
+            .ForEach((Entity entity, int entityInQueryIndex, in SpawnCollectableComponent scc) =>
+            {
+                Unity.Mathematics.Random rand = new Unity.Mathematics.Random(30);
+                for (var x = 0; x < scc.amount; x++)
+                {
+                    Entity entityInstance = commandBuffer.Instantiate(entityInQueryIndex, scc.prefab);
+
+                    // Set a random spawn position
+                    float spawnPosX = rand.NextFloat(-5.0f, 5.0f);
+                    float spawnPosZ = rand.NextFloat(-5.0f, 5.0f);
+
+                    // Place the instantiated on a random position
+                    float3 position = new float3(spawnPosX, -0.5f, spawnPosZ);
+                    commandBuffer.SetComponent(entityInQueryIndex, entityInstance, new Translation { Value = position });
+                }
+                commandBuffer.DestroyEntity(entityInQueryIndex, entity);
+            }).ScheduleParallel();
+
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
